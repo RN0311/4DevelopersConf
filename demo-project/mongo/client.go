50cce23f2c250bcf2e1f94af31d7f90e6a1e6071
Rashmi Nagpal <rashminagpal.nagpal@gmail.com>
2021-10-26 17:25:50 -0700

    Added mongo client in Go

--- demo-project/mongo/client.go
+++ demo-project/mongo/client.go
@@ -1,1 +1,23 @@
+package mongo
 
+import (
+	"github.com/globalsign/mgo"
+)
+
+type Client struct {
+	session *mgo.Session
+}
+
+func NewClient(connectionString string) (*Client, error) {
+	session, err := mgo.Dial(connectionString)
+	if err != nil {
+		return nil, err
+	}
+	return &Client{session: session}, nil
+}
+
+func (c *Client) NewSession() *mgo.Session {
+	newSession := c.session.Copy()
+	newSession.SetMode(mgo.Strong, true)
+	return newSession
+}
